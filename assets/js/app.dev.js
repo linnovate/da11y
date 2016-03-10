cd1cc062d6740876587c1c642b674572430774c2
Aron <astein@linnovate.net>
2016-03-10 16:42:40 +0200

    fix base style

--- assets/js/app.dev.js
+++ assets/js/app.dev.js
@@ -35,8 +35,6 @@
 ( function( $, window, document, undefined ) {
 	'use strict';
 	
-
-
 	var Pojo_Accessibility_App = {
 		cache: {
 			$document: $( document ),
@@ -46,49 +44,14 @@
 		cacheElements: function() {
 			this.cache.$toolbar = $( '#pojo-a11y-toolbar' );
 			this.cache.$toolbarLinks = this.cache.$toolbar.find( 'div.pojo-a11y-toolbar-overlay a.pojo-a11y-toolbar-link' );
-			this.cache.$btnToolbarToggle = this.cache.$toolbar.find( 'div.pojo-a11y-toolbar-toggle > a' );
 			this.cache.$btnBackgrounGroup = this.cache.$toolbar.find( 'a.pojo-a11y-btn-background-group' );
-			this.cache.$skipToContent = $( '#pojo-a11y-skip-content' );
 			this.cache.$body = $( 'body' );
 		},
 
-		buildElements: function() {
-			// Move the `toolbar/skip to content` to top
-			this.cache.$body.prepend( this.cache.$toolbar );
-			this.cache.$body.prepend( this.cache.$skipToContent );
-		},
-
-		bindEvents: function() {
-			var $self = this;
-
-			$self.cache.$btnToolbarToggle.on( 'click', function( event ) {
-				event.preventDefault();
-				
-				$self.cache.$toolbar.toggleClass( 'pojo-a11y-toolbar-open' );
-				
-				if ( $self.cache.$toolbar.hasClass( 'pojo-a11y-toolbar-open' ) ) {
-					$self.cache.$toolbarLinks.attr( 'tabindex', '0' );
-				} else {
-					$self.cache.$toolbarLinks.attr( 'tabindex', '-1' );
-				}
-			} )
-
-				.on( 'focus', function( event ) {
-					event.preventDefault();
-
-					$self.cache.$toolbar.addClass( 'pojo-a11y-toolbar-open' );
-					$self.cache.$toolbarLinks.attr( 'tabindex', '0' );
-				} );
-			
-			$self.bindToolbarButtons();
-		},
-		
 		bindToolbarButtons: function() {
 			var $self = this;
 
-			$self.currentFontSize = 120;
-
-			$self.cache.$toolbar.find( 'a.pojo-a11y-btn-resize-font' ).on( 'click', function( event ) {
+			function resizeFont( event ) {
 				event.preventDefault();
 
 				var MAX_SIZE = 200,
@@ -113,9 +76,9 @@
 					$self.cache.$toolbar.find( 'a.pojo-a11y-btn-resize-plus' ).removeClass( 'active' );
 				}
 				
-			} );
-
-			$self.cache.$btnBackgrounGroup.on( 'click', function( event ) {
+			}
+			
+			function backgrounGroup( event ) {
 				event.preventDefault();
 				
 				var currentAction = $( this ).data( 'action' ),
@@ -140,23 +103,21 @@
 				if ( ! isButtonActive ) {
 					$( this ).addClass( 'active' );
 				}
-			} );
+			}
 
-			$self.cache.$toolbar.find( 'a.pojo-a11y-btn-links-underline' ).on( 'click', function( event ) {
+			function linksUnderline( event ) {
 				event.preventDefault();
-
 				$self.cache.$body.toggleClass( 'pojo-a11y-links-underline' );
 				$( this ).toggleClass( 'active' );
-			} );
+			}
 
-			$self.cache.$toolbar.find( 'a.pojo-a11y-btn-readable-font' ).on( 'click', function( event ) {
+			function readableFont( event ) {
 				event.preventDefault();
-
 				$self.cache.$body.toggleClass( 'pojo-a11y-readable-font' );
 				$( this ).toggleClass( 'active' );
-			} );
+			}
 
-			$self.cache.$toolbar.find( 'a.pojo-a11y-btn-reset' ).on( 'click', function( event ) {
+			function reset( event ) {
 				event.preventDefault();
 
 				$self.cache.$body.removeClass( 'pojo-a11y-grayscale pojo-a11y-high-contrast pojo-a11y-negative-contrast pojo-a11y-light-background pojo-a11y-links-underline pojo-a11y-readable-font' );
@@ -165,36 +126,69 @@
 				var MIN_SIZE = 120;
 				$self.cache.$body.removeClass( 'pojo-a11y-resize-font-' + $self.currentFontSize );
 				$self.currentFontSize = MIN_SIZE;
-			} );
+			}
+
+			$self.currentFontSize = 120;
+			$self.cache.$toolbar.find( 'a.pojo-a11y-btn-resize-font' ).click(resizeFont);
+			$self.cache.$btnBackgrounGroup.click(backgrounGroup);
+			$self.cache.$toolbar.find( 'a.pojo-a11y-btn-links-underline' ).click(linksUnderline);
+			$self.cache.$toolbar.find( 'a.pojo-a11y-btn-readable-font' ).click(readableFont);
+			$self.cache.$toolbar.find( 'a.pojo-a11y-btn-reset' ).click(reset);
 		},
 
-		handleGlobalOptions: function() {
-			/*if ( '1' === PojoA11yOptions.focusable ) {
-				this.cache.$body.addClass( 'pojo-a11y-focusable' );
-			}
-			
-			if ( '1' === PojoA11yOptions.remove_link_target ) {
-				$( 'a[target="_blank"]' ).attr( 'target', '' );
-			}
-			
-			if ( '1' === PojoA11yOptions.add_role_links ) {
-				$( 'a' ).attr( 'role', 'link' );
-			}*/
-		},
 		
 		init: function() {
 			this.cacheElements();
-			this.buildElements();
-			this.bindEvents();
-			this.handleGlobalOptions();
+			this.bindToolbarButtons();
 		}
 	};
 
 	$( document ).ready( function( $ ) {
+		var plugin;
+		if(plugin = document.querySelector('#da11y-plugin')) {
+			plugin.innerHTML = '<div id="da11y-toggle"></div><div id="da11y-options"></div>';
+		}
+
+		$('#da11y-toggle').attr("tabindex",0);
 		
-		$('body .access').append('<nav id="pojo-a11y-toolbar" class="pojo-a11y-toolbar-left" role="navigation"><div class="pojo-a11y-toolbar-toggle"><a class="pojo-a11y-toolbar-link pojo-a11y-toolbar-toggle-link" href="javascript:void(0);" title="כלי נגישות"><span class="sr-only" title="פתח סרגל נגישות"></span><i class="fa fa-wheelchair"></i></a></div><div class="pojo-a11y-toolbar-overlay"><div class="pojo-a11y-toolbar-inner"><p class="pojo-a11y-toolbar-title">כלי נגישות</p><ul class="pojo-a11y-toolbar-items pojo-a11y-tools"><li class="pojo-a11y-toolbar-item"><a href="#" class="pojo-a11y-toolbar-link pojo-a11y-btn-resize-font pojo-a11y-btn-resize-plus" data-action="plus" tabindex="-1">הגדל טקסט</a></li><li class="pojo-a11y-toolbar-item"><a href="#" class="pojo-a11y-toolbar-link pojo-a11y-btn-resize-font pojo-a11y-btn-resize-minus" data-action="minus" tabindex="-1">הקטן טקסט</a></li><li class="pojo-a11y-toolbar-item"><a href="#" class="pojo-a11y-toolbar-link pojo-a11y-btn-background-group pojo-a11y-btn-grayscale" data-action="grayscale" tabindex="-1">גווני אפור</a></li><li class="pojo-a11y-toolbar-item"><a href="#" class="pojo-a11y-toolbar-link pojo-a11y-btn-background-group pojo-a11y-btn-high-contrast" data-action="high_contrast" tabindex="-1">ניגודיות גבוהה</a></li><li class="pojo-a11y-toolbar-item"><a href="#" class="pojo-a11y-toolbar-link pojo-a11y-btn-background-group pojo-a11y-btn-negative-contrast" data-action="negative_contrast" tabindex="-1">ניגודיות הפוכה</a></li><li class="pojo-a11y-toolbar-item"><a href="#" class="pojo-a11y-toolbar-link pojo-a11y-btn-background-group pojo-a11y-btn-light-bg" data-action="light-bg" tabindex="-1">רקע בהיר</a></li><li class="pojo-a11y-toolbar-item"><a href="#" class="pojo-a11y-toolbar-link pojo-a11y-btn-links-underline" tabindex="-1">הדגשת קישורים</a></li><li class="pojo-a11y-toolbar-item"><a href="#" class="pojo-a11y-toolbar-link pojo-a11y-btn-readable-font" tabindex="-1">פונט קריא</a></li><li class="pojo-a11y-toolbar-item"><a href="#" class="pojo-a11y-toolbar-link pojo-a11y-btn-reset" tabindex="-1">איפוס</a></li></ul></div></div></nav>');
+		function da11yToggle(){
+			var o = $('#da11y-options');
+			o.hasClass("active") ? o.removeClass("active") : o.addClass("active");
+
+			var t = $(this);
+			t.hasClass("active") ? t.removeClass("active") : t.addClass("active");
+		}
+		function da11yToggleActive(){
+			$('#da11y-options').addClass("active");
+			$('#da11y-toggle').addClass("active");
+		}
+		
+		$('#da11y-toggle').click(da11yToggle);
+// 	    $('#da11y-toggle').focus(da11yToggleActive);
+
+	    $('#da11y-options').append(
+			'<nav id="pojo-a11y-toolbar" class="pojo-a11y-toolbar-left" role="navigation"><ul>' +
+				'<li class="pojo-a11y-toolbar-item">' +
+				'<a tabindex="0" href="#" class="pojo-a11y-toolbar-link pojo-a11y-btn-resize-font pojo-a11y-btn-resize-plus" data-action="plus" tabindex="-1">הגדל טקסט</a></li>' +
+				'<li class="pojo-a11y-toolbar-item">' +
+				'<a tabindex="0" href="#" class="pojo-a11y-toolbar-link pojo-a11y-btn-resize-font pojo-a11y-btn-resize-minus" data-action="minus" tabindex="-1">הקטן טקסט</a><' +
+				'/li><li class="pojo-a11y-toolbar-item">' +
+				'<a tabindex="0" href="#" class="pojo-a11y-toolbar-link pojo-a11y-btn-background-group pojo-a11y-btn-grayscale" data-action="grayscale" tabindex="-1">גווני אפור</a></li>' +
+				'<li class="pojo-a11y-toolbar-item">' +
+				'<a tabindex="0" href="#" class="pojo-a11y-toolbar-link pojo-a11y-btn-background-group pojo-a11y-btn-high-contrast" data-action="high_contrast" tabindex="-1">ניגודיות גבוהה</a></li>' +
+				'<li class="pojo-a11y-toolbar-item">' +
+				'<a tabindex="0" href="#" class="pojo-a11y-toolbar-link pojo-a11y-btn-background-group pojo-a11y-btn-negative-contrast" data-action="negative_contrast" tabindex="-1">ניגודיות הפוכה</a></li>' +
+				'<li class="pojo-a11y-toolbar-item">' +
+				'<a tabindex="0" href="#" class="pojo-a11y-toolbar-link pojo-a11y-btn-background-group pojo-a11y-btn-light-bg" data-action="light-bg" tabindex="-1">רקע בהיר</a></li>' +
+				'<li class="pojo-a11y-toolbar-item">' +
+				'<a tabindex="0" href="#" class="pojo-a11y-toolbar-link pojo-a11y-btn-links-underline" tabindex="-1">הדגשת קישורים</a></li>' +
+				'<li class="pojo-a11y-toolbar-item">' +
+				'<a tabindex="0" href="#" class="pojo-a11y-toolbar-link pojo-a11y-btn-readable-font" tabindex="-1">פונט קריא</a>' +
+				'</li><li class="pojo-a11y-toolbar-item">' +
+				'<a tabindex="0" href="#" class="pojo-a11y-toolbar-link pojo-a11y-btn-reset" tabindex="-1">איפוס</a></li>' +
+			'</ul></nav>');
 
 		Pojo_Accessibility_App.init();
-	} );
+	});
 
 }( jQuery, window, document ) );
